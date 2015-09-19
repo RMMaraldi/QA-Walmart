64e77ebb1379ede289cbcaf264c30aa69b9c3e6d
RMMaraldi <rafaelmaraldi@gmail.com>
2015-09-19 06:40:36 -0300

    Update Invalid_CEP.rb

--- features/step_definitions/Invalid_CEP.rb
+++ features/step_definitions/Invalid_CEP.rb
@@ -1,1 +1,11 @@
+Given(/^I Access the Correio's API send as parameter an invalid CEP$/) do
+	visit 'http://cep.correiocontrol.com.br/00000000.json'
+end
 
+When (/^I don't receive in response the fields: bairro, logradouro, cep, uf and localidade$/) do
+	expect(page).to have_content 'erro'
+end
+
+When (/^I should see the message: Invalid CEP$/) do
+	print 'Invalid CEP'
+end
